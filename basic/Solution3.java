229ed40ec15d22e51e7fcbfb3b86f0d4740cdd45
raju <rajusharmadata123@gmail.com>
2024-09-08 14:10:23 +0530

    add

--- basic/Solution3.java
+++ basic/Solution3.java
@@ -1,46 +1,3 @@
-/*
- Java has 8 primitive data types; char, boolean, byte, short, int, long, float, and double. For this exercise, we'll work with the primitives used to hold integer values (byte, short, int, and long):
-
-A byte is an 8-bit signed integer.
-A short is a 16-bit signed integer.
-An int is a 32-bit signed integer.
-A long is a 64-bit signed integer.
-Given an input integer, you must determine which primitive data types are capable of properly storing that input.
-
-To get you started, a portion of the solution is provided for you in the editor.
-
-Input Format
-
-The first line contains an integer,T , denoting the number of test cases.
-Each test case, T, is comprised of a single line with an integer,n , which can be arbitrarily large or small.
-
-Output Format
-
-For each input variable n and appropriate primitive data type, you must determine if the given primitives are capable of storing it. If yes, then print:
-Sample Input
-
-5
--150
-150000
-1500000000
-213333333333333333333333333333333333
--100000000000000
-Sample Output
-
--150 can be fitted in:
-* short
-* int
-* long
-150000 can be fitted in:
-* int
-* long
-1500000000 can be fitted in:
-* int
-* long
-213333333333333333333333333333333333 can't be fitted anywhere.
--100000000000000 can be fitted in:
-* long
- */
 import java.util.*;
 import java.io.*;
 
@@ -61,23 +18,23 @@ public class Solution3 {
             {
                 long x=sc.nextLong();
                 System.out.println(x+" can be fitted in:");
-                if(x>=-128 && x<=127){
+                if(x>=Byte.MIN_VALUE && x<=Byte.MAX_VALUE){
                 System.out.println("* byte");
                 }
-                if(x>=-32768 && x<=32768 ){
+                if(x>=Short.MIN_VALUE && x<=Short.MAX_VALUE){
                     System.out.println("* short");
                 }
-                 if( x>=-2147483647 && x<2147483647){
+                 if( x>=Integer.MIN_VALUE && x<=Integer.MAX_VALUE){
                     System.out.println("* int");
                 }
-                if (x==x){
+                if (x>=Long.MIN_VALUE && x<= Long.MAX_VALUE){
                     System.out.println("* long");
                 }
                 //Complete the code
             }
             catch(Exception e)
             {
-                System.out.println(sc.next()+" can't be fitted anywhere.");
+                System.out.println(sc.next()+" can't b`e fitted anywhere.");
             }
 
         }
